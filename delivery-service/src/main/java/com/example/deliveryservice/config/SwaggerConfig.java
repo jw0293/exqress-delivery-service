9207b769adb90f8b76434b21116d6d2ba831e762
Kim jung won <k38836919@gmail.com>
2023-05-08 17:00:46 +0900

    Swagger 설정

--- delivery-service/src/main/java/com/example/deliveryservice/config/SwaggerConfig.java
+++ delivery-service/src/main/java/com/example/deliveryservice/config/SwaggerConfig.java
@@ -1,2 +1,62 @@
-package com.example.deliveryservice.config;public class SwaggerConfig {
+package com.example.deliveryservice.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import java.util.Arrays;
+import java.util.List;
+
+@OpenAPIDefinition(
+        info = @Info(title = "Delivery-Service API 명세서",
+                description = "배송 기사 어플 서비스 API 명세서",
+                version = "v1"))
+@RequiredArgsConstructor
+@Configuration
+public class SwaggerConfig{
+
+
+//    @Bean
+//    public Docket api() {
+//        return new Docket(DocumentationType.OAS_30)
+//                //.securityContexts(Arrays.asList(securityContext())) // 추가
+//                //.securitySchemes(Arrays.asList(apiKey())) // 추가
+//                .useDefaultResponseMessages(false)
+//                .select()
+//                .apis(RequestHandlerSelectors.basePackage("com.example.deliveryservice.controller"))
+//                .paths(PathSelectors.any())
+//                .build()
+//                .apiInfo(apiInfo());
+//    }
+
+//    private SecurityContext securityContext() {
+//        return SecurityContext.builder()
+//                .securityReferences(defaultAuth())
+//                .build();
+//    }
+//
+//    private List<SecurityReference> defaultAuth() {
+//        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+//        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+//        authorizationScopes[0] = authorizationScope;
+//        return Arrays.asList(new SecurityReference("Authorization", (springfox.documentation.service.AuthorizationScope[]) authorizationScopes));
+//    }
+//
+//    // 추가
+//    private ApiKey apiKey() {
+//        return new ApiKey("Authorization", "Authorization", "header");
+//    }
+//
+
+//    private ApiInfo apiInfo() {
+//        return new ApiInfoBuilder()
+//                .title("Delivery-Service Backend API")
+//                .description("Backend API 문서")
+//                .version("1.0")
+//                .build();
+//    }
 }
