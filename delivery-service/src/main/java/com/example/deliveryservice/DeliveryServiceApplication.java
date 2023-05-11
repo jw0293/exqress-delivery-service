8206f5e785a24c7e9ad6e63e12d0059c9ad0f76f
Kim jung won <k38836919@gmail.com>
2023-05-11 21:37:18 +0900

    feat : Config Client 설정

--- delivery-service/src/main/java/com/example/deliveryservice/DeliveryServiceApplication.java
+++ delivery-service/src/main/java/com/example/deliveryservice/DeliveryServiceApplication.java
@@ -1,7 +1,9 @@
 package com.example.deliveryservice;
 
+import com.example.deliveryservice.config.MyConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -9,9 +11,11 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableConfigurationProperties(MyConfig.class)
 public class DeliveryServiceApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(DeliveryServiceApplication.class, args);
 	}
 
--- delivery-service/src/main/java/com/example/deliveryservice/config/MyConfig.java
+++ delivery-service/src/main/java/com/example/deliveryservice/config/MyConfig.java
@@ -1,2 +1,17 @@
-package com.example.deliveryservice.config;public class MyConfig {
+package com.example.deliveryservice.config;
+
+import lombok.*;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+
+@Setter
+@Getter
+@ConfigurationProperties("com.example")
+@RefreshScope
+@ToString
+public class MyConfig {
+
+    private String profile;
+    private String region;
+
 }
