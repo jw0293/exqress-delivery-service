22a6bb2720d7dfb2c1261678929eb266e8df530c
Kim jung won <k38836919@gmail.com>
2023-05-16 20:59:44 +0900

    refact : 로그인 기능 Security 제외, Controller 구현

--- delivery-service/src/main/java/com/example/deliveryservice/config/WebSecurityConfig.java
+++ delivery-service/src/main/java/com/example/deliveryservice/config/WebSecurityConfig.java
@@ -2,28 +2,35 @@ package com.example.deliveryservice.config;
 
 import com.example.deliveryservice.security.AuthenticationFilter;
 import com.example.deliveryservice.security.CustomAuthenticationProvider;
-import com.example.deliveryservice.service.DeliveryService;
-import com.example.deliveryservice.service.DeliveryServiceImpl;
-import com.example.deliveryservice.service.TokenService;
-import com.example.deliveryservice.service.TokenServiceImpl;
+import com.example.deliveryservice.service.*;
+import com.example.deliveryservice.utils.CookieUtils;
+import com.example.deliveryservice.utils.TokenUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.filter.CorsFilter;
 
 @RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
 
-    private final TokenServiceImpl tokenService;
+    private final TokenUtils tokenService;
+    private final CookieUtils cookieUtils;
+    private final CorsFilter corsFilter;
+    //private final HandlerExceptionResolver resolver;
+
     private final DeliveryServiceImpl deliveryService;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final RedisTemplate<String, Object> redisTemplate;
     private final AuthenticationConfiguration authenticationConfiguration;
 
     @Bean
@@ -32,13 +39,18 @@ public class WebSecurityConfig {
                 csrf().disable().headers()
                 .frameOptions().sameOrigin()
                 .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .httpBasic().disable()
+                .formLogin().disable()
+                .addFilter(corsFilter)
                 .csrf().ignoringRequestMatchers("/h2-console/**").disable()
-                .authorizeHttpRequests().requestMatchers("/h2-console/**").permitAll()
-                .and()
-                .authorizeHttpRequests()
-                .requestMatchers("/**").permitAll()
-                .and()
-                .addFilter(getAuthenticationFilter());
+                .authorizeHttpRequests().requestMatchers("/h2-console/**").permitAll();
+
+        http.
+                authorizeHttpRequests()
+                .anyRequest().permitAll();
 
         return http.build();
     }
@@ -54,8 +66,8 @@ public class WebSecurityConfig {
     }
 
     private AuthenticationFilter getAuthenticationFilter() throws Exception {
-        AuthenticationFilter authenticationFilter = new AuthenticationFilter(tokenService, deliveryService);
-        authenticationFilter.setFilterProcessesUrl("/delivery/login");
+        AuthenticationFilter authenticationFilter = new AuthenticationFilter(tokenService, deliveryService, redisTemplate);
+        //authenticationFilter.setFilterProcessesUrl("/delivery/login");
         authenticationFilter.setAuthenticationManager(authenticationManager());
 
         return authenticationFilter;
