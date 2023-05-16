61476050bd4fbcb82655379c06441161ee1c5ec1
Kim jung won <k38836919@gmail.com>
2023-05-16 21:09:05 +0900

    QR코드 테이블 생성

--- delivery-service/src/main/java/com/example/deliveryservice/entity/QRcode.java
+++ delivery-service/src/main/java/com/example/deliveryservice/entity/QRcode.java
@@ -1,2 +1,41 @@
-package com.example.deliveryservice.entity;public class QRcode {
+package com.example.deliveryservice.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.ColumnDefault;
+
+@Data
+@Entity
+@Table
+public class QRcode {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "qrcode_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private String qrId;
+
+    @Column(nullable = false)
+    private String receiverName;
+
+    @Column(nullable = false)
+    private String receiverPhoneNumber;
+
+    @Column(nullable = false)
+    private String productName;
+
+    @Column(nullable = false)
+    private String invoiceNo;
+
+    @Column(nullable = false)
+    @ColumnDefault("false")
+    private boolean isComplete;
+
+    @Embedded
+    private Address address;
+
+    @ManyToOne
+    @JoinColumn(name = "delivery_entity_id")
+    private DeliveryEntity deliveryEntity;
 }
