2e9b3c4d69a918e9b796cf3f2a5d7c7508f1ad20
Kim jung won <k38836919@gmail.com>
2023-05-16 21:06:49 +0900

    feat : 에러 처리를 위한 Controller

--- delivery-service/src/main/java/com/example/deliveryservice/controller/ErrorController.java
+++ delivery-service/src/main/java/com/example/deliveryservice/controller/ErrorController.java
@@ -1,2 +1,22 @@
-package com.example.deliveryservice.controller;public class ErrorController {
+package com.example.deliveryservice.controller;
+
+import com.example.deliveryservice.StatusEnum;
+import com.example.deliveryservice.vo.response.ResponseData;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/error")
+public class ErrorController {
+
+    @GetMapping("/unauthorized")
+    public String throwUnauthorizedError(){
+        return "리다이렉트 시켰으니 여기서 Reissue로 다시 POST로 날려줘!";
+    }
 }
