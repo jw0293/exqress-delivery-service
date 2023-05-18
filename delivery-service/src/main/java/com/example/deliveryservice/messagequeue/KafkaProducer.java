a50b1749945504251432822f5f0ce61f69a51537
Kim jung won <k38836919@gmail.com>
2023-05-18 16:10:29 +0900

    build : deconnect Kafka

--- delivery-service/src/main/java/com/example/deliveryservice/messagequeue/KafkaProducer.java
+++ delivery-service/src/main/java/com/example/deliveryservice/messagequeue/KafkaProducer.java
@@ -1,15 +1,15 @@
-package com.example.deliveryservice.messagequeue;
-
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.stereotype.Service;
-
-@RequiredArgsConstructor
-@Service
-@Slf4j
-public class KafkaProducer {
-
-    private final KafkaTemplate<String, String> kafkaTemplate;
-
-}
+//package com.example.deliveryservice.messagequeue;
+//
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.kafka.core.KafkaTemplate;
+//import org.springframework.stereotype.Service;
+//
+//@RequiredArgsConstructor
+//@Service
+//@Slf4j
+//public class KafkaProducer {
+//
+//    private final KafkaTemplate<String, String> kafkaTemplate;
+//
+//}
