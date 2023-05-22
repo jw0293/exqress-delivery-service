a0c612fc985b8680d51c48ae85e76ece11afbbb8
Kim jung won <k38836919@gmail.com>
2023-05-22 19:29:26 +0900

    blind

--- delivery-service/src/main/java/com/example/deliveryservice/dto/kafka/DeliveryInfoWithQRId.java
+++ delivery-service/src/main/java/com/example/deliveryservice/dto/kafka/DeliveryInfoWithQRId.java
@@ -1,2 +1,12 @@
-package com.example.deliveryservice.dto.kafka;public class DeliveryInfoWithQRId {
-}
+//package com.example.deliveryservice.dto.kafka;
+//
+//import lombok.Data;
+//
+//@Data
+//public class DeliveryInfoWithQRId {
+//
+//    private String qrId;
+//    private String deliveryName;
+//    private String deliveryPhoneNumber;
+//    private String state = "배송 시작";
+//}
